df02d35758d3cc325f8f3fac32f00ddd176bc372
nithe.nithesh@gmail.com <nithe.nithesh@gmail.com>
2020-11-03 16:31:55 +0530

    first commit

---
+++ src/root.component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./root.component.css";
+
+export default function Root() {
+  return (
+    <div className="container2">
+      <p> Content from  App5</p>
+    </div>
+  );
+}
+// export default function Root(props) {
+//   return <section>{props.name} is mounted!</section>;
+// }
